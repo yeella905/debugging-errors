3b1b5aba03902ac389894f1cac52d45d4f667b55
yeella905 <yeella905@gmail.com>
2024-11-28 01:51:58 -0500

    reverse debug

--- reverse.js
+++ reverse.js
@@ -1,8 +1,8 @@
 var input = process.argv[2];
 
+function reverse(original) {
+  return original.split('').reverse().join('');
+}
+
 if (input)
   console.log(reverse(input));
-
-function reverse(original) {
-  return orignal.split('').reverseList().join('');
-}
