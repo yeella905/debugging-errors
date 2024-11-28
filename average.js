7ee9c2ea48f99e9d3a678415ba5a7313fc55c383
Ella Ye <yeella905@gmail.com>
2024-11-28 00:20:10 -0500

    Initial commit

---
+++ average.js
@@ -0,0 +1,10 @@
+function average(list) {
+    var sum = 0;
+  
+    for (var num of list) {
+      sum ++ num;
+    }
+  
+    return sum / list.length;
+  
+console.log(average([3, 5, 7]));
---
+++ reverse.js
@@ -0,0 +1,8 @@
+var input = process.argv[2];
+
+if (input)
+  console.log(reverse(input));
+
+function reverse(original) {
+  return orignal.split('').reverseList().join('');
+}
